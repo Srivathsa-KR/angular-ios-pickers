9e37fd9155e4b23e91408bea566be60fc4300dfb
Srivathsa K R <krsrivathsa98@gmail.com>
2023-09-04 17:27:48 +0530

    timepicker added to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ALLOWED_DATES_ENUM } from './components/ios-datepicker/ios-datepicker.constants';
+import { TIME_FORMAT_ENUM } from './components/ios-timepicker/ios-timepicker.constants';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,10 @@ import { ALLOWED_DATES_ENUM } from './components/ios-datepicker/ios-datepicker.c
 })
 export class AppComponent {
   selectedFutureDate = new Date();
-  selectedPastDate = new Date()
+  selectedPastDate = new Date();
+  dateFor12HourTime = new Date();
+  dateFor24HourTime = new Date();
 
   readonly ALLOWED_DATES = ALLOWED_DATES_ENUM;
+  readonly TIME_FORMAT = TIME_FORMAT_ENUM;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { IosDatepickerComponent } from './components/ios-datepicker/ios-datepicker.component';
 import { IosWheelpickerComponent } from './components/ios-wheelpicker/ios-wheelpicker.component';
+import { IosTimepickerComponent } from './components/ios-timepicker/ios-timepicker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IosDatepickerComponent,
+    IosTimepickerComponent,
     IosWheelpickerComponent
   ],
   imports: [

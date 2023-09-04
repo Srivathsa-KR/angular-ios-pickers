90253bd0e07618562bdbc7adc34e917abcc6b50d
Srivathsa K R <krsrivathsa98@gmail.com>
2023-09-04 17:27:10 +0530

    time picker changes

--- src/app/components/ios-timepicker/ios-timepicker.component.ts
+++ src/app/components/ios-timepicker/ios-timepicker.component.ts
@@ -1,7 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output, OnChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {
   TWELVE_HOUR_LIMIT,
-  TWELVE_HOURS_ARRAY_FORMATTED,
+  TWELVE_HOURS_ARRAY_REARRANGED,
   TWENTY_FOUR_HOURS_ARRAY,
   MINUTE_ARRAY,
   MERIDIEM_ENUM,
@@ -10,8 +10,9 @@ import {
   getValue,
   getTwentyFourHourRepresentation,
   MERIDIEM_TYPE,
+  TIME_FORMAT_ENUM
 } from './ios-timepicker.constants';
-import { TIME_FORMAT_ENUM } from './../../../modules/flows/flow-creation/flow-design/flow-action-settings/all-action-settings/action-timepicker-data.constants';
+// import {  } from './../../../modules/flows/flow-creation/flow-design/flow-action-settings/all-action-settings/action-timepicker-data.constants';
 import { DEFAULT_SETTING_ENUM } from '../ios-wheelpicker/ios-wheelpicker.component';
 
 @Component({
@@ -23,61 +24,49 @@ export class IosTimepickerComponent implements OnInit {
   @Input('date') selectedDate: Date;
   @Input('timeFormat') timeFormat: string;
   @Input('intervalStep') intervalStep: number | null;
-  @Output('onChange') timeChangeEmitter = new EventEmitter<Date>(true);
+  @Output('dateChange') timeChangeEmitter = new EventEmitter<Date>(true);
 
   public HOURS_ARRAY: number[];
   public MERIDIEM_ARRAY = MERIDIEM_ARRAY;
   public MIN_ARRAY_STATE: Array<number>;
-  public meridiem: MERIDIEM_TYPE;
-  public TIME_FORMAT_ENUM = TIME_FORMAT_ENUM;
-  public TWELVE_HOUR_LIMIT = TWELVE_HOUR_LIMIT;
+  public meridiemOfSelectedDate: MERIDIEM_ENUM;
+  readonly TIME_FORMAT_ENUM = TIME_FORMAT_ENUM;
+  readonly TWELVE_HOUR_LIMIT = TWELVE_HOUR_LIMIT;
   public DEFAULT_TO_STRATEGY = DEFAULT_SETTING_ENUM;
 
   constructor() {}
 
   ngOnInit() {
-    this.meridiem = getSelectedMeridiem(this.selectedDate) as MERIDIEM_TYPE;
+    this.meridiemOfSelectedDate = getSelectedMeridiem(this.selectedDate);
     if (this.timeFormat === TIME_FORMAT_ENUM.TWELVE_HOUR_FORMAT) {
-      this.HOURS_ARRAY = TWELVE_HOURS_ARRAY_FORMATTED;
-    } else if (this.timeFormat === TIME_FORMAT_ENUM.TWENTY_FOUR_HOUR_FORMAT) {
+      this.HOURS_ARRAY = TWELVE_HOURS_ARRAY_REARRANGED;
+    } else {
       this.HOURS_ARRAY = TWENTY_FOUR_HOURS_ARRAY;
     }
-    if (!this.intervalStep) {
-      this.MIN_ARRAY_STATE = MINUTE_ARRAY;
-    } else {
+    if (this.intervalStep) {
       this.MIN_ARRAY_STATE = MINUTE_ARRAY.filter((value) => value % this.intervalStep === 0);
+    } else {
+      this.MIN_ARRAY_STATE = MINUTE_ARRAY;
     }
   }
 
   public onHourChange(changedHourIndex: number) {
     if (this.timeFormat === TIME_FORMAT_ENUM.TWELVE_HOUR_FORMAT) {
-      if (this.meridiem === MERIDIEM_ENUM.PM && TWELVE_HOURS_ARRAY_FORMATTED[changedHourIndex] !== TWELVE_HOUR_LIMIT) {
+      if (this.meridiemOfSelectedDate === MERIDIEM_ENUM.PM && TWELVE_HOURS_ARRAY_REARRANGED[changedHourIndex] !== TWELVE_HOUR_LIMIT) {
         this.updateDate(
-          this.selectedDate.setHours(getTwentyFourHourRepresentation(TWELVE_HOURS_ARRAY_FORMATTED[changedHourIndex])),
+          this.selectedDate.setHours(getTwentyFourHourRepresentation(TWELVE_HOURS_ARRAY_REARRANGED[changedHourIndex]))
         );
       } else if (
-        this.meridiem === MERIDIEM_ENUM.AM &&
-        TWELVE_HOURS_ARRAY_FORMATTED[changedHourIndex] === TWELVE_HOUR_LIMIT
+        this.meridiemOfSelectedDate === MERIDIEM_ENUM.AM &&
+        TWELVE_HOURS_ARRAY_REARRANGED[changedHourIndex] === TWELVE_HOUR_LIMIT
       ) {
         this.updateDate(this.selectedDate.setHours(0));
       } else {
-        this.updateDate(this.selectedDate.setHours(TWELVE_HOURS_ARRAY_FORMATTED[changedHourIndex]));
+        this.updateDate(this.selectedDate.setHours(TWELVE_HOURS_ARRAY_REARRANGED[changedHourIndex]));
       }
-    } else if (this.timeFormat === TIME_FORMAT_ENUM.TWENTY_FOUR_HOUR_FORMAT) {
+    } else {
       this.updateDate(this.selectedDate.setHours(TWENTY_FOUR_HOURS_ARRAY[changedHourIndex]));
     }
-  }
-
-  private correctTimeOnMeridiemChange() {
-    if (this.meridiem == MERIDIEM_ENUM.AM && this.selectedDate.getHours() >= TWELVE_HOUR_LIMIT) {
-      this.updateDate(this.selectedDate.setHours(this.selectedDate.getHours() - TWELVE_HOUR_LIMIT));
-    } else if (this.meridiem == MERIDIEM_ENUM.PM && this.selectedDate.getHours() < TWELVE_HOUR_LIMIT) {
-      this.updateDate(this.selectedDate.setHours(this.selectedDate.getHours() + TWELVE_HOUR_LIMIT));
-    }
-  }
-
-  private updateDate(timestamp: number) {
-    this.timeChangeEmitter.emit(new Date(timestamp));
   }
 
   public onMinuteChange(changedMinuteIndex: number) {
@@ -85,15 +74,27 @@ export class IosTimepickerComponent implements OnInit {
   }
 
   public onMeridiemChange(changedMeridiemIndex: number) {
-    this.meridiem = MERIDIEM_ARRAY[changedMeridiemIndex] as MERIDIEM_TYPE;
-    this.correctTimeOnMeridiemChange();
+    this.meridiemOfSelectedDate = MERIDIEM_ARRAY[changedMeridiemIndex];
+    //Meridiem has changed from PM to AM
+    if (this.meridiemOfSelectedDate == MERIDIEM_ENUM.AM) {
+      this.updateDate(this.selectedDate.setHours(this.selectedDate.getHours() - TWELVE_HOUR_LIMIT));
+    }
+    //Meridiem has changed from AM to PM
+    else {
+      this.updateDate(this.selectedDate.setHours(this.selectedDate.getHours() + TWELVE_HOUR_LIMIT));
+    }
+    // this.correctTimeOnMeridiemChange();
   }
 
   public getIndexOfSelectedHour() {
     if (this.timeFormat === TIME_FORMAT_ENUM.TWELVE_HOUR_FORMAT) {
-      return TWELVE_HOURS_ARRAY_FORMATTED.indexOf(getValue(this.selectedDate.getHours(), TWELVE_HOUR_LIMIT));
-    } else if (this.timeFormat === TIME_FORMAT_ENUM.TWENTY_FOUR_HOUR_FORMAT) {
+      return TWELVE_HOURS_ARRAY_REARRANGED.indexOf(getValue(this.selectedDate.getHours(), TWELVE_HOUR_LIMIT));
+    } else {
       return TWENTY_FOUR_HOURS_ARRAY.indexOf(this.selectedDate.getHours());
     }
+  }
+
+  private updateDate(timestamp: number) {
+    this.timeChangeEmitter.emit(new Date(timestamp));
   }
 }
